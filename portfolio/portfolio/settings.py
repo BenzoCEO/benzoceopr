b5321b5812093d552db15b60acfdd0a56a91d282
DESKTOP-FCUBGB7\ENTER <benardodey57@gmail.com>
2025-09-05 18:35:19 +0100

    Add wsgi.py for deployment

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -36,13 +36,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Application definition
 
 INSTALLED_APPS = [
+    'portfolio.main',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'main',
+    
 ]
 
 MIDDLEWARE = [
